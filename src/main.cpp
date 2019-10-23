585debf9f36e024bdcb75cd88724c248d8d26e47
KT <reg@kovnet.hu>
2019-10-23 11:35:58 +0200

    villogás hozzáadása

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
 
+const uint8_t LED = 13;
+
 void setup() {
-  // put your setup code here, to run once:
+    pinMode(LED, OUTPUT);
+    digitalWrite(LED, LOW);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+    digitalWrite(LED, HIGH);
+    delay(1000);
+    digitalWrite(LED, LOW);
+    delay(1000);
 }
